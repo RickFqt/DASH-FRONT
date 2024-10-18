7bda79419db4993b88e368028234f757a92cc9be
RickFqt <henriquefouquet@gmail.com>
2024-10-18 09:36:51 -0300

    WIP: Adiciona layout inicial da tela de prontuario

--- src/app/prontuario-view/prontuario-view.component.ts
+++ src/app/prontuario-view/prontuario-view.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-prontuario-view',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './prontuario-view.component.html',
   styleUrl: './prontuario-view.component.css'
 })
 export class ProntuarioViewComponent {
+  buttonSrc: string = 'button.png';
 
+  onHoverButton() {
+    this.buttonSrc = 'button_hover.png';
+  }
+
+  onLeaveButton() {
+    this.buttonSrc = 'button.png';
+  }
 }
